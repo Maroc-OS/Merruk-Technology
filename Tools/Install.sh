990ef8206d29a4df7ab656b4ad134f60eff94389
Yahya Lmallas <merruk.company@gmail.com>
2012-08-16 03:30:27 +0000

    Fix for Kernel Packing

--- Tools/Install.sh
+++ Tools/Install.sh
@@ -2,6 +2,10 @@ rm -Rf unpack
 rm -Rf boot
 
 mkdir -p unpack
+mkdir -p source_img
+cd sources_img
+tar -xvf stock_kernel*
+cd ..
 tools/unpackbootimg -i source_img/boot.img -o unpack
 
 mkdir -p boot
@@ -16,13 +20,6 @@ cd ../
 
 mkdir boot/system/etc
 mkdir boot/system/etc/init.d
-
-#cp Worked/sbin/su boot/sbin/
-#cp Worked/sbin/busybox boot/sbin/
-#cp Worked/sbin/init.data boot/sbin/
-#cp Worked/sbin/sysinit.rc boot/sbin/
-#cp Worked/sbin/bmlunlock boot/sbin/
-#cp Worked/system/etc/init.d/* boot/system/etc/init.d/
 
 cp ../MerrukTechnology_Output/zImage ./unpack/
 
