2e4b5349b031b7667ae137a824ceeec01aaef620
Yahya Lmallas <merruk.company@gmail.com>
2012-08-17 02:40:05 +0000

    Compress.sh now makes PDA.[param].timestamps.tar :
    How To Use :
    
        ./Compress.sh [Parameter]
            - merruk  =         Use Merruk Technology Kernel
            - stock   =         Use Merruk Technology Kernel
        Please spesifie a parameter of listed above

--- Tools/Compress.sh
+++ Tools/Compress.sh
@@ -1,14 +1,72 @@
-#For Stock Kernel
-#tools/mkbootfs boot | gzip > unpack/boot.img-ramdisk.gz
+###################################################################
+# Merruk Technology Install Script For A new Installation/Update  #
+# For InitRamFS (ramdisk) Envirement .                            #
+###################################################################
+# Author    : Yahya Lmallas                                       #
+# E-Mail    : y.lmallas@merruk.ma, merruk.company@gmail.com       #
+# Company   : Merruk Technology, SARL.                            #
+# Web-Site  : www.merruk.ma                                       #
+# UserName  : Maroc-OS                                            #
+###################################################################
 
-#For MerrukTechnology Kernel
-tools/mkbootfs boot | lzma > unpack/boot.img-ramdisk.gz
+# Setup the Nedded Directories.
 
+if [ test ! -d "target_img" ];
+  then
+    mkdir -p target_img
+fi
+sync
+
+# Commpress the RamDisk.
+# (TODO : Test the Support of Parameters Ex: ramdisk= "merruk" or "stock" )
+
+function help
+{
+    echo "Positional parameter 1 is empty !"
+    echo "How To Use :"
+    echo "./Compress.sh [Parameter]"
+    echo "  - merruk  =     Use Merruk Technology InitRamFS"
+    echo "  - stock   =     Use Merruk Technology InitRamFS"
+    echo "Please spesifie a parameter of listed above"
+    exit 1
+} # end help
+
+if [ "$1" == "" ];
+  then
+    $(help)
+else
+  echo "Commpressing Kernel RamDisk..."
+      if [ "$1" == "merruk" ];
+        then
+          echo ""
+          echo "Merruk Technology RamDisk."
+          echo ""
+          gzip -dc ../unpack/boot.img-ramdisk.gz | cpio -i
+      elif [ "$1" == "stock" ];
+        then
+          echo ""
+          echo "Samsung Stock RamDisk."
+          echo ""
+          tools/mkbootfs boot | lzma > unpack/boot.img-ramdisk.gz
+      else
+        $(help)
+    fi
+fi
+
+# Copy The Compiled Kernel From the Output Directory.
 cp ../MerrukTechnology_Output/zImage ./unpack/
 
-tools/mkbootimg --kernel unpack/zImage --ramdisk unpack/boot.img-ramdisk.gz -o target_img/boot.img --base `cat unpack/boot.img-base`
-cd target_img
-rm -Rf boot.tar
-tar cvf boot.tar *
-cd ../
+# Making The New Kernel :)
+  echo ""
+  echo "Making the New Kernel..."
+  echo ""
 
+tools/mkbootimg --kernel unpack/zImage --ramdisk unpack/boot.img-ramdisk.gz -o target_img/boot.img --base `cat unpack/boot.img-base`
+
+# Remove the old TAR Compressed Kernel.
+  cd target_img
+  rm -Rf *.tar
+
+# Make a Tarball for the Constructed Kernel Image (boot.img).
+  tar cvf PDA.$1.$$.tar *
+  cd ../
